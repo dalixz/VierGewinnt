af64f0f08fe2a06c0c4ce24c1bc4a9e8032185b1
Schule <schule@macbook.fritz.box>
2018-11-03 14:57:57 +0100

    Anpassungen UI

--- VierGewinnt/VierGewinnt/VierGewinntLogic.py
+++ VierGewinnt/VierGewinnt/VierGewinntLogic.py
@@ -2,10 +2,24 @@ class MainLogic:
     def __init__(self):
         self.started = False
         self.use_ki = False
+        self.playername1 = None
+        self.playername2 = None
+        self.current_player_id = 1
 
     def get_started(self):
         return self.started
     
-    def start_game(self, use_ki):
+    def start_game(self, use_ki, playername1, playername2):
         self.use_ki = use_ki
+        self.playername1 = playername1
+        if use_ki:
+            self.playername2 = "Computer"
+        else:
+            self.playername2 = playername2
         self.started = True
+
+    def get_current_player_name(self):
+        if self.current_player_id == 1:
+            return self.playername1
+        if self.current_player_id == 2:
+            return self.playername2
--- VierGewinnt/VierGewinnt/VierGewinntUI.py
+++ VierGewinnt/VierGewinnt/VierGewinntUI.py
@@ -7,10 +7,11 @@ class VierGewinntWindow:
         self.logic = VierGewinntLogic
         self.window = None
         self.current_screen_name = None
-        self.default_background_color = "#3f89ff"
+        self.default_background_color = "#bcd6ff"
         self.yellow = "#faff00"
         self.red = "#ff4f4f"
         self.holes = [None] * 7 # 7 horizontal - leeres array initailisieren
+        self.playername2_backup = None
 
 
     def generate_mainloop(self):
@@ -42,17 +43,43 @@ class VierGewinntWindow:
         l1 = Label(self.window, text="Vier Gewinnt", font=(None, 32))
         l1.pack()
 
+        #Blinkeffekt Überschrift
         self.window.after(500, self.blink_headline, l1)
 
+        #Author / Version
         Label(self.window, text="v" + VierGewinnt.__version__ + " by " + VierGewinnt.__author__, font=(None, 14)).pack()
-        Label(self.window, text="Spielmodus:", font=(None, 12)).pack(pady=20)
+        
+        #---------------- Spielmodus -----------------
+        Label(self.window, text="Spielmodus", font=(None, 18)).pack(pady=10)
 
+        #Spielmodus; 1 = 2 Spieler; 2 = 1 Spieler (gegen KI)
         self.play_mode = IntVar(None, 1)
 
-        Radiobutton(self.window, text="2 Spieler", variable=self.play_mode, value=1).pack()
-        Radiobutton(self.window, text="Gegen KI", variable=self.play_mode, value=2).pack()
+        #In Frame auslagern für korrekte positionierung der radiobuttons
+        self.play_mode_frame = Frame(self.window)
+        self.play_mode_frame.pack()
 
-        Button(self.window, text="Spiel starten", command=self.on_startgame_click).pack(pady=40)
+        Radiobutton(self.play_mode_frame, text="2 Spieler", variable=self.play_mode, value=1, command=self.on_playmode_change).grid(column=0, row=0, sticky="W")
+        Radiobutton(self.play_mode_frame, text="1 Spielger (gegen KI)", variable=self.play_mode, value=2, command=self.on_playmode_change).grid(column=0, row=1, sticky="W")
+
+        #----------------- Spielernamen ----------------
+        Label(self.window, text="Spielernamen", font=(None, 18)).pack(pady=10)
+
+        self.playername_frame = Frame(self.window)
+        self.playername_frame.pack()
+
+        Label(self.playername_frame, text="Spieler 1:").grid(column=0, row=0)
+        Label(self.playername_frame, text="Spieler 2:").grid(column=0, row=1)
+
+        self.textbox_playername1 = Text(self.playername_frame, height=1, width=15)
+        self.textbox_playername1.grid(column=1, row=0)
+        self.textbox_playername1.insert(END, "Player1")
+
+        self.textbox_playername2 = Text(self.playername_frame, height=1, width=15)
+        self.textbox_playername2.grid(column=1, row=1)
+        self.textbox_playername2.insert(END, "Player2")
+
+        Button(self.window, text="Spiel starten", command=self.on_startgame_click).pack(pady=18)
 
 
     def blink_headline(self, label):
@@ -69,8 +96,12 @@ class VierGewinntWindow:
             label.config(fg=self.yellow)
 
     def on_startgame_click(self):
+        #Problem mit tkinter: Am Anfang des Textes befindet sich immer ein \n. kA wieso
+        player1_name = self.textbox_playername1.get("1.0", END).replace("\n", "")
+        player2_name = self.textbox_playername2.get("1.0", END).replace("\n", "")
+
         #Logik starten - use_ki = self.play_mode == 2
-        self.logic.start_game(self.play_mode == 2)
+        self.logic.start_game(self.play_mode == 2, player1_name, player2_name)
 
     def draw_gamefield(self):
         #Fenster leeren
@@ -92,7 +123,6 @@ class VierGewinntWindow:
             h = [None] * 6 # vertical
 
             for v in h:
-                print("create canvas")
                 v = Canvas(self.game_field_frame, width=40, height=40)
                 v.grid(row=row_offset, column=column_offset)
                 v.create_oval(10, 10, 42, 42)
@@ -106,4 +136,17 @@ class VierGewinntWindow:
             widget.destroy()
 
     def set_current_play_name(self):
-        print("")
+        self.player_headline.config(text=self.logic.get_current_player_name() + " ist dran!")
+
+    def on_playmode_change(self):
+        if self.play_mode.get() == 1:
+            self.textbox_playername2.config(state="normal")
+            self.textbox_playername2.delete('1.0', END)
+            self.textbox_playername2.insert(END, self.playername2_backup)
+
+        if self.play_mode.get() == 2:
+            self.playername2_backup = self.textbox_playername2.get("1.0", END)
+            self.textbox_playername2.delete('1.0', END)
+            self.textbox_playername2.insert(END, "Computer")
+            self.textbox_playername2.config(state="disabled")
+
